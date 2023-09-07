3e05304330ddc2493d78ae303d20d213ee6ffc9a
EC2 Default User <ec2-user@ip-172-31-1-157.ap-southeast-2.compute.internal>
2023-09-07 07:47:17 +0000

    bookers2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+
+Refile.secret_key = 'db526bc91f8ac01bff6579cf24981890f314e4fb72e9fe533d64accc1a787faec6af379020cd3e8dc45d21457ed60bc95130663f0458ca68b991d20d23ba6818'
