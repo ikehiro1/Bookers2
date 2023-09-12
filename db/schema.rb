aa12bf4d018655ae17d3b444ff99505e28a9cc79
EC2 Default User <ec2-user@ip-172-31-1-157.ap-southeast-2.compute.internal>
2023-09-12 05:29:50 +0000

    bookers2[finish]

--- db/schema.rb
+++ db/schema.rb
@@ -67,3 +67,4 @@ ActiveRecord::Schema.define(version: 2023_09_06_073241) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
+
